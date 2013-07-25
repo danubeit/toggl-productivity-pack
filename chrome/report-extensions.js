4b6df7ef0a198ecd145892b4fec4718c6feca1ad
Oliver Hanappi <oliver.hanappi@danubeit.com>
2013-07-25 12:17:03 +0200

    Fixed bug that due to asynchronous loading of the reports page the filter controls were not found anymore.

--- chrome/report-extensions.js
+++ chrome/report-extensions.js
@@ -1,20 +1,29 @@
 function setState(context, state) {
-	$('li:not(.opt-group) input[type="checkbox"][value!="no-tags"]', $(context).parent()).attr('checked', !state);
-	$('li:not(.opt-group) input[type="checkbox"][value!="no-tags"]', $(context).parent()).trigger('click');
+	$('li:not(.opt-group) input[type="checkbox"][value!="0"]', $(context).parent()).attr('checked', !state);
+	$('li:not(.opt-group) input[type="checkbox"][value!="0"]', $(context).parent()).trigger('click');
+}
+
+function setUpExtension() {
+	if ($('.ui-multiselect-menu').length == 0) {
+		setTimeout(setUpExtension, 500);
+	}
+	else {
+		$('.ui-multiselect-menu').each(function() {
+			if ($('input[type="checkbox"]', this).length > 0) {
+				$(this).prepend('<a href="#" class="report-extensions-all">All</a> | <a href="#" class="report-extensions-none">None</a>');
+			}
+		});
+		
+		$('.report-extensions-all').on('click', function() {
+			setState(this, true);
+		});
+		
+		$('.report-extensions-none').on('click', function() {
+			setState(this, false);
+		});
+	}
 }
 
 $(function() {
-	$('.ui-multiselect-menu').each(function() {
-		if ($('input[type="checkbox"]', this).length > 0) {
-			$(this).prepend('<a href="#" class="report-extensions-all">All</a> | <a href="#" class="report-extensions-none">None</a>');
-		}
-	});
-	
-	$('.report-extensions-all').on('click', function() {
-		setState(this, true);
-	});
-	
-	$('.report-extensions-none').on('click', function() {
-		setState(this, false);
-	});
+	setUpExtension();
 });
